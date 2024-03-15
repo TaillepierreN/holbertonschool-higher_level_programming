638abc77108f85f714eb1b1c6543b60cc366c5a5
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-15 17:06:24 +0100

    added 9-full_creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,27 @@
+-- Creates a table second_table {id INT, name VARCHAR(256), score INT}and add multiples rows
+CREATE TABLE
+	IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+INSERT INTO
+	second_table
+SET
+	id = 1,
+	name = 'John',
+	score = 10;
+INSERT INTO
+	second_table
+SET
+	id = 2,
+	name = 'Alex',
+	score = 3;
+INSERT INTO
+	second_table
+SET
+	id = 3,
+	name = 'Bob',
+	score = 14;
+INSERT INTO
+	second_table
+SET
+	id = 3,
+	name = 'George',
+	score = 8;
