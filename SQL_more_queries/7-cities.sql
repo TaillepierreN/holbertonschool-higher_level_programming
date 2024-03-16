002077bc0184dcb1a7ae92658e2d7c44b1453a41
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-16 13:32:22 +0100

    add 7-cities

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,10 @@
+-- Create the database hbtn_0d_usa
+--containing the table cities (id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, 
+--state_id INT NOT NULL FOREIGN KEY(states(id)), name VARCHAR(256) NOT NULL)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
+	id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	FOREIGN KEY (state_id) REFERENCES states(id)
+)
