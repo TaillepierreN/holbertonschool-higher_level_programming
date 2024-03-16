071127731169a322c29b449a3e9e5f5bbf112d86
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-16 11:30:06 +0100

    add 6-states

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,8 @@
+-- Create the db hbtn_0d_usa
+-- containing table states(id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL)
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE
+	IF NOT EXISTS hbtn_0d_usa.states (
+		id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		name VARCHAR(256) NOT NULL
+	)
