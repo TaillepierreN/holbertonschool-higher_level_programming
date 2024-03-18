37da8d62e66e500f04c07bb69a6edfa6abe3810e
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-18 10:51:34 +0100

    changed selected and cities.state_id

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,8 +1,8 @@
 -- List all the cities in California that can be found in hbtn_0d_usa
-SELECT cities.id, cities.name
+SELECT id, name
 FROM hbtn_0d_usa.cities
 WHERE
-    state_id = (
+    cities.state_id = (
         SELECT id
         FROM hbtn_0d_usa.states
         WHERE
