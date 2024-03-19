11d1fc5e6cc29cdd84b3b27b2649d808f1e71ba6
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 11:46:59 +0100

    0- change database to db

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     dbname database name to connect to
     """
     db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], database=sys.argv[3])
+                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cursor.fetchall()
