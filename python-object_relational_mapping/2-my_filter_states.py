fb7e2701a4691810fb5831532e9d7ccc23af1443
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 14:34:25 +0100

    2- added binary

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute(
         "SELECT * FROM states WHERE \
-        name LIKE '{}' ORDER BY id ASC".format(argv[4]))
+        name LIKE BINARY '{}' ORDER BY id ASC".format(argv[4]))
     rows = cur.fetchall()
     for row in rows:
         print(row)
