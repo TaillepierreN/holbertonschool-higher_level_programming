0505697cc27c0e2f96bd3db114f98a728b3f49b5
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 12:00:00 +0100

    0- rewrote the code without dos2unix to try

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
-import MySQLdb
-import sys
+#!/bin/python3
 """
 Script that list all states from database hbtn_0e_0_usa
 """
+import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
     """
@@ -12,12 +13,15 @@ if __name__ == "__main__":
     password: password for the db
     dbname database name to connect to
     """
-    db = MySQLdb.connect(host='localhost', port=3306,
-                         user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    cursor.close()
+    cur.close()
     db.close()
