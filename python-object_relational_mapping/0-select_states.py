48978a0192e1b6d14ca0996e3ad7ae3616d5d95b
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 12:23:08 +0100

    0- back to original codes

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,35 +1,23 @@
 #!/usr/bin/python3
-""" script that lists all states
-from the database hbtn_0e_0_usa
+"""
+Script that list all states from database hbtn_0e_0_usa
 """
 
 import MySQLdb
 from sys import argv
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     """
     Main execution block
-    Connects to the database and retrieves the states
+    connect to mysql database and list states by ID ascending
     """
-    # Connect to the MySQL database using the provided arguments
-    db = MySQLdb.connect(
-        host="localhost",  # Hostname of the database server
-        user=argv[1],     # Username obtained from command-line argument
-        port=3306,        # Port number of the database server
-        passwd=argv[2],   # Password obtained from command-line argument
-        db=argv[3]
-    )       # Database name obtained from command-line argument
-
-    # Create a cursor object to execute SQL queries
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
-
-    # Execute an SQL query to select all rows
-    # from the 'states' table
     cur.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all rows returned by the query
     rows = cur.fetchall()
-
-    # Iterate through the fetched rows and print each row
     for row in rows:
         print(row)
