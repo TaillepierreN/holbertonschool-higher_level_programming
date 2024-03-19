d7831dab1413cd40258122cfc8e5fa8657205b52
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 15:08:02 +0100

    added 4-cities_by_state

---
+++ python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Script that list all states from database hbtn_0e_0_usa starting with N
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Main execution block
+    connect to mysql database and list states starting with N by ID ascending.
+    """
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute(
+        "SELECT cities.id, cities.name, states.name FROM cities \
+        JOIN states ON states.id = cities.state_id ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
