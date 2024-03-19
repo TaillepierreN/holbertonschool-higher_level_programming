8965c4c5133b71eee1b24ddd239118e18671623b
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 12:09:16 +0100

    0 import argv directly from sys

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -2,9 +2,9 @@
 """
 Script that list all states from database hbtn_0e_0_usa
 """
-import MySQLdb
-import sys
 
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
     """
@@ -13,9 +13,9 @@ if __name__ == "__main__":
     """
     db = MySQLdb.connect(host='localhost',
                          port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
     rows = cur.fetchall()
