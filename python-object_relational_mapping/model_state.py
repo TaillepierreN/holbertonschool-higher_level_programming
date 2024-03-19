7c2cc55375bbc75ca3ea77f6fada546d2b4c222c
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-19 16:09:47 +0100

    Added model_state and 6-model_state py and sql dependency

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module for the State class, and an instance of declarative_base()
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    Class State
+    """
+    __tablename__ = 'states'
+    id = Column(Integer, autoincrement=True, unique=True,
+                nullable=False, primary_key=True)
+    name = Column(String(128), nullable=False)
