309b342507aa47e759732c8e29f5d17b854072f6
Nicolas <81607410+TaillepierreN@users.noreply.github.com>
2024-03-20 11:16:41 +0100

    Added 8-model_state_fetch_first

---
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+Script to list all states in hbtn_0e_6_usa
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from sys import argv
+
+if __name__ == "__main__":
+    """
+    Main execution code
+    Check Argv
+    Create an engine to connect to a MySQL server
+    Create with Factory function sessionmaker a work area
+    with engine binded foR execution of SQL statements
+    QuEry the State class representing a table
+    anD print the first entry in the table
+
+    """
+    if len(argv) == 4:
+        username, password, db_name = argv[1:4]
+
+        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+            username, password, db_name), pool_pre_ping=True)
+
+        DBsession = sessionmaker(bind=engine)
+        session = DBsession()
+
+        state = session.query(State).order_by(State.id).first()
+        print("{}: {}".format(state.id, state.name))
+
+    else:
+        print("Usage: {} <username> <password> <db_name>".format(argv[0]))
